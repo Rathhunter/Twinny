5141e423536e54d2cc8f60662e35b79db004efd8
Rathhunter <timothybush@blueyonder.co.uk>
2017-03-16 17:14:12 +0000

    turn and face

--- New Unity Project/Assets/_Scripts/Shoot.cs
+++ New Unity Project/Assets/_Scripts/Shoot.cs
@@ -5,6 +5,10 @@ public class Shoot : MonoBehaviour {
     GameObject LegsRef;
     public GameObject BulletRef;
     public GameObject FirePoint;
+    public GameObject FirePoint1;
+    public GameObject FirePoint2;
+    public GameObject FirePoint3;
+    public GameObject FirePoint4;
     float reload;
     bool TractorUse;
     int WeaponUsed;
@@ -44,6 +48,14 @@ public class Shoot : MonoBehaviour {
             {
                 TractorUse = true;
                 VacuumRef = (GameObject)Instantiate(BulletRef, new Vector3(FirePoint.transform.position.x, FirePoint.transform.position.y, 0), this.transform.rotation);
+            }
+            if (WeaponUsed == 2 && reload >= firerate)
+            {
+                Instantiate(BulletRef, new Vector3(FirePoint1.transform.position.x, FirePoint1.transform.position.y, 0), FirePoint1.transform.rotation);
+                Instantiate(BulletRef, new Vector3(FirePoint2.transform.position.x, FirePoint2.transform.position.y, 0), FirePoint2.transform.rotation);
+                Instantiate(BulletRef, new Vector3(FirePoint3.transform.position.x, FirePoint3.transform.position.y, 0), FirePoint3.transform.rotation);
+                Instantiate(BulletRef, new Vector3(FirePoint4.transform.position.x, FirePoint4.transform.position.y, 0), FirePoint4.transform.rotation);
+                reload = 0;
             }
         }
         if (Input.GetAxis(LegsRef.GetComponent<Character>().GetRightTrigger()) == 0)
---
+++ New Unity Project/Assets/_Scripts/ShrinkOverTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrinkOverTime : MonoBehaviour {
+
+    public int StartScale;
+    public int endscale;
+    public float CurrentSize;
+    public float Shrinkspeed;
+    void Start()
+    {
+        CurrentSize = StartScale;
+    }
+    void Update()
+    {
+        CurrentSize -= Time.deltaTime * Shrinkspeed;
+        this.transform.localScale = new Vector3(CurrentSize, CurrentSize, 0);
+        if (CurrentSize <= 0)
+            DestroyImmediate(this.gameObject);
+    }
+}
