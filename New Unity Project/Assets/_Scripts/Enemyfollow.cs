f863c67c0e4c4514bfd9c683da7445242b96e17f
Rathhunter <timothybush@blueyonder.co.uk>
2017-03-17 23:44:55 +0000

    ch ch changes

--- New Unity Project/Assets/_Scripts/Enemyfollow.cs
+++ New Unity Project/Assets/_Scripts/Enemyfollow.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class Enemyfollow : MonoBehaviour {
-    public int EncounterMax;
-    public int EncounterMin;
-    public int FleeRange;
+    public float EncounterMax;
+    public float EncounterMin;
+    public float FleeRange;
     GameObject PlayerRef;
     float PositiveX;
     float NegativeX;
@@ -14,11 +14,18 @@ public class Enemyfollow : MonoBehaviour {
     float MinimumPosY;
     float MinimumNegX;
     float MinimumNegY;
+    float MoveSpeed;
+    public float Speed;
     // Use this for initialization
     void Start ()
     {
         PlayerRef = GameObject.Find("Prefab_Platform");
-	PositiveX = this.transform.position.x + EncounterMax;
+	
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    PositiveX = this.transform.position.x + EncounterMax;
     NegativeX = this.transform.position.x - EncounterMax;
     PositiveY = this.transform.position.y + EncounterMax;
     NegativeY = this.transform.position.y - EncounterMax;
@@ -26,13 +33,13 @@ public class Enemyfollow : MonoBehaviour {
     MinimumPosY = this.transform.position.y + EncounterMin;
     MinimumNegX = this.transform.position.x - EncounterMin;
     MinimumNegY = this.transform.position.y - EncounterMin;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	if(PlayerRef.transform.position.x <= PositiveX && PlayerRef.transform.position.x >= MinimumPosX && PlayerRef.transform.position.y <= PositiveY && PlayerRef.transform.position.y >= MinimumPosY)
+        MoveSpeed = Time.deltaTime * Speed;
+	if(PlayerRef.transform.position.x <= PositiveX && PlayerRef.transform.position.x >= NegativeX && PlayerRef.transform.position.y <= PositiveY && PlayerRef.transform.position.y >= NegativeY)
         {
-
+           if (PlayerRef.transform.position.x > MinimumPosX || PlayerRef.transform.position.x < MinimumNegX && PlayerRef.transform.position.y > MinimumPosY || PlayerRef.transform.position.y < MinimumNegY)
+           {
+                transform.position = Vector3.MoveTowards(transform.position, PlayerRef.transform.position, MoveSpeed);
+           }
         }
 	}
 }
